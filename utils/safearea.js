7db08c723f6ccc1ef72783ce4f3a638c48ef9dc5
Jibrin Victor <jibrinvictorachile@gmail.com>
2022-12-14 11:29:51 +0100

    change to meta data

---
+++ utils/safearea.js
@@ -0,0 +1,19 @@
+import { SafeAreaView,StyleSheet,Platform,StatusBar} from "react-native";
+
+export function SafeArea ({children}) {
+    console.log()
+   return (
+    <SafeAreaView style={styles.areaContainer}>
+        {children}
+    </SafeAreaView>
+   )
+}
+
+const styles = StyleSheet.create({
+    areaContainer:{
+        flex:1,
+        paddingHorizontal:8,
+        marginTop:Platform.OS =='android'? StatusBar.currentHeight :null,
+        
+    }
+})
